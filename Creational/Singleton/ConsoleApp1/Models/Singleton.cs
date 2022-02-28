46b477cfbb2efa3524f6c675b003a90cb97b1726
Rana Sobhy <Rana.Sobhy@integrant.com>
2022-02-28 19:01:37 +0200

    Add Singleton Pattern

---
+++ Creational/Singleton/ConsoleApp1/Models/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Creational/Singleton/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1.Models;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
